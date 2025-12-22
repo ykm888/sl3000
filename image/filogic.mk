7891906e146093ba6d06e833ec15f17a5837836e
ykm888 <165190180+ykm888@users.noreply.github.com>
2025-12-22 13:00:55 +0800

    Update filogic.mk

--- image/filogic.mk
+++ image/filogic.mk
@@ -1,26 +1,9 @@
 define Device/s13000_emmc
-  DEVICE_VENDOR := S
-  DEVICE_MODEL := S13000
-  DEVICE_VARIANT := eMMC
+  DEVICE_VENDOR := S13000
+  DEVICE_MODEL := S13000 eMMC
   DEVICE_DTS := mt7981b-s13000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES += s13000,emmc
-
-  BLOCKSIZE := 128k
-  PAGESIZE := 2k
-  IMAGE_SIZE := 128m
-
-  IMAGES := sysupgrade.bin
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-
-  DEVICE_PACKAGES := \
-    kmod-mt7915e \
-    kmod-mt7531 \
-    kmod-mmc \
-    kmod-mmc-mtk \
-    kmod-mediatek_hnat \
-    block-mount \
-    kmod-fs-ext4 \
-    e2fsprogs
+  DEVICE_PACKAGES := kmod-mt7981-firmware \
+                     kmod-usb3 kmod-usb2 \
+                     kmod-leds-gpio kmod-gpio-button-hotplug
 endef
 TARGET_DEVICES += s13000_emmc
