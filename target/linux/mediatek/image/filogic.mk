5dd57f0c3accbdb37d8938ceb64bb7fdac19d657
ykm888 <165190180+ykm888@users.noreply.github.com>
2025-12-24 12:35:58 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -4,6 +4,26 @@ define Device/sl3000
   DEVICE_DTS := mt7981b-sl3000-emmc
   IMAGE_SIZE := 256m
   SUPPORTED_DEVICES := sl3000
+  DEVICE_PACKAGES := \
+    kmod-mt7981-firmware \
+    kmod-mt76 \
+    kmod-mt76-core \
+    kmod-mt76-connac \
+    kmod-leds-gpio \
+    kmod-gpio-button-hotplug \
+    block-mount \
+    kmod-fs-ext4 \
+    kmod-mmc \
+    kmod-mmc-mtk \
+    luci \
+    luci-base \
+    luci-mod-admin-full \
+    luci-theme-bootstrap \
+    luci-app-opkg \
+    luci-ssl \
+    luci-i18n-base-zh-cn \
+    dnsmasq-full \
+    ppp-mod-pppoe
 endef
 
 TARGET_DEVICES += sl3000
