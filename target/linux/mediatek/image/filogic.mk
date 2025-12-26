925c53bd82d4aef1ed02066ebf647d3f38ee8a14
ykm888 <165190180+ykm888@users.noreply.github.com>
2025-12-26 13:07:15 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -574,6 +574,45 @@ define Device/cetron_ct3003-ubootmod
 endef
 TARGET_DEVICES += cetron_ct3003-ubootmod
 
+define Device/sl3000
+  DEVICE_VENDOR := SL
+  DEVICE_MODEL := 3000
+  DEVICE_DTS := mt7981b-sl3000-emmc
+  IMAGE_SIZE := 268435456
+
+  DEVICE_PACKAGES := \
+    kmod-mt7981-firmware \
+    kmod-mt76 \
+    kmod-mt76-core \
+    kmod-mt76-connac \
+    wpad-basic-mbedtls \
+    iwinfo \
+    kmod-leds-gpio \
+    kmod-gpio-button-hotplug \
+    kmod-mediatek_eth \
+    kmod-mediatek_hnat \
+    kmod-mt7531 \
+    block-mount \
+    kmod-fs-ext4 \
+    kmod-mmc \
+    kmod-mmc-mtk \
+    e2fsprogs \
+    kmod-nls-base \
+    dnsmasq-full \
+    ppp \
+    ppp-mod-pppoe \
+    uhttpd \
+    uhttpd-mod-ubus \
+    libustream-mbedtls \
+    rpcd \
+    rpcd-mod-rrdns \
+    luci \
+    luci-ssl \
+    luci-i18n-base-zh-cn
+endef
+
+TARGET_DEVICES += sl3000
+
 define Device/cmcc_a10-stock
   DEVICE_VENDOR := CMCC
   DEVICE_MODEL := A10
